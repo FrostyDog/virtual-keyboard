8f0b6fe7f6d44460ad50617a8f9f77436005b298
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-06-02 21:43:56 +0200

    fix: eslint & textArea path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Example of the links. Easy configurable and extandable by adding new elements or deleting ones
-import engKeys from './modules/eng-keys.js';
-import Keyboard from './modules/keyboard.js';
-import Textarea from './modules/Textarea.js';
+import engKeys from './modules/eng-keys';
+import Keyboard from './modules/keyboard';
+import Textarea from './modules/textarea';
 
 // Load new Menu with links
-window.onload = new Textarea(document.getElementById("body"));
-window.onload = new Keyboard(document.getElementById("body"), engKeys);
+window.onload = new Textarea(document.getElementById('body'));
+window.onload = new Keyboard(document.getElementById('body'), engKeys);
